12d386db6a5e2b3606f7dfd8abf8f9a69556e974
grzegorzwozniakjg <gw.trying.code@gmail.com>
2022-10-24 21:14:43 +0200

    04 ciag dalszy

--- ruby_fundamentals/04_definiowanie_metod.rb
+++ ruby_fundamentals/04_definiowanie_metod.rb
@@ -11,4 +11,16 @@ def buy_cookie_2(cookie_name)
     p "kupiles ciastko - #{cookie_name}"
 end
 
-buy_cookie_2(cookie_name"torcik")
+buy_cookie_2("torcik")
+
+def buy_meal(meal_name = "tofucznica") #parametr domyslny
+    p "Kupiles posilek - #{meal_name}"
+end
+
+buy_meal("pizza")
+
+def buy_car(brand, car_color)
+    p "Kupiles #{brand} ktory ma kolor #{car_color}"
+end
+
+buy_car("Toyota", "Czarny")
